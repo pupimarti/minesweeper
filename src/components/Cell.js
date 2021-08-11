588415442f5017c974631deff07600181e7f8086
Juan Agustín Marti <pupi_marti@hotmail.com>
2021-08-10 21:04:20 -0300

    add mines and values cell

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React from 'react';
+import { View, Text } from 'react-native';
 
 export default function Cell({ value, isMine }) {
   return (
@@ -7,15 +7,15 @@ export default function Cell({ value, isMine }) {
       style={{
         width: 30,
         height: 30,
-        backgroundColor: "grey",
-        alignItems: "center",
-        justifyContent: "center",
+        backgroundColor: isMine ? 'red' : 'grey',
+        alignItems: 'center',
+        justifyContent: 'center',
         borderWidth: 1,
         margin: 1,
       }}
     >
-      <Text style={{ color: "#FFF", fontWeight: "bold" }}>
-        {isMine ? "X" : value}
+      <Text style={{ color: '#FFF', fontWeight: 'bold' }}>
+        {isMine ? 'X' : value}
       </Text>
     </View>
   );
--- src/utils/board.js
+++ src/utils/board.js
@@ -1,11 +1,42 @@
-export const createBoard = () => {
-  let new_board = [[]];
-  for (let i = 0; i < 10; i++) {
+export const createBoard = (size = 10, quantityMines = 10) => {
+  let new_board = [];
+  for (let y = 0; y < size; y++) {
     let column = [];
-    for (let x = 0; x < 10; x++) {
+    for (let x = 0; x < size; x++) {
       column.push({ value: 0, isMine: false });
     }
     new_board.push(column);
   }
+
+  let mines = [];
+
+  while (mines.length < quantityMines) {
+    const y = Math.floor(Math.random() * size - 1) + 1;
+    const x = Math.floor(Math.random() * size - 1) + 1;
+    if (!new_board[y][x].isMine) {
+      new_board[y][x].isMine = true;
+      mines.push({ y, x });
+    }
+  }
+
+  mines.forEach(({ x, y }) => {
+    let index_y = y - 1;
+    let index_x = x - 1;
+    while (index_y <= y + 1) {
+      if (index_y >= 0 && index_y < size) {
+        while (index_x <= x + 1) {
+          if (index_x >= 0 && index_x < size) {
+            if (!new_board[index_y][index_x].isMine)
+              new_board[index_y][index_x].value++;
+          }
+          index_x++;
+        }
+      }
+      index_x = x - 1;
+
+      index_y++;
+    }
+  });
+
   return new_board;
 };
