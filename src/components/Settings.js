1eff2d4fbb6a61381a290d393ba31452ea199f96
Juan Agustín Marti <pupi_marti@hotmail.com>
2021-08-16 19:20:05 -0300

    checks size & quantity mines

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,19 +1,41 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { View, Text, TextInput, Button } from 'react-native';
 import GameContext from '../context/GameContext';
 import { STATE_GAME } from '../utils/constraints';
 
 export default function Settings() {
-  const { size, setSize, newBoard, stateGame } = useContext(GameContext);
+  const {
+    size,
+    setSize,
+    newBoard,
+    stateGame,
+    quantityMines,
+    setQuantityMines,
+  } = useContext(GameContext);
 
-  const handleChangeSize = (val) => {
-    if (!isNaN(val)) setSize(val);
+  const handleChangeSize = (val) => !isNaN(val) && setSize(val);
+
+  const handleChangeMines = (val) => !isNaN(val) && setQuantityMines(val);
+
+  const [error, setError] = useState(null);
+
+  const handleAccept = () => {
+    setError(null);
+    if (isNaN(size) || size < 5) {
+      setError('El tamaño no puede ser menor que 5.');
+      return;
+    }
+    if (isNaN(quantityMines) || quantityMines >= size * size) {
+      setError('La cantidad de minas debe ser mayor al tamaño');
+      return;
+    }
+    newBoard();
   };
 
   if (stateGame === STATE_GAME.PLAY) return <View></View>;
 
   return (
-    <View>
+    <View style={{ flex: 1, alignItems: 'center' }}>
       <Text>Tamaño del tablero:</Text>
       <TextInput
         style={{ borderWidth: 1, marginBottom: 5 }}
@@ -21,7 +43,15 @@ export default function Settings() {
         onChangeText={handleChangeSize}
         keyboardType={'numeric'}
       />
-      <Button title="Aceptar" onPress={newBoard} />
+      <Text>Cantidad de minas:</Text>
+      <TextInput
+        style={{ borderWidth: 1, marginBottom: 5 }}
+        value={quantityMines}
+        onChangeText={handleChangeMines}
+        keyboardType={'numeric'}
+      />
+      {error && <Text style={{ color: 'black' }}>Error: {error}</Text>}
+      <Button title="Aceptar" onPress={handleAccept} />
     </View>
   );
 }
